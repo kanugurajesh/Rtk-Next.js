566924c9d8b2b7aa72e4f6e6aa3c3ca3ee6d63b0
kanugurajesh <kanugurajesh3@gmail.com>
2024-04-13 16:53:30 +0530

    Adding the redux store in nextjs

---
+++ components/Counter.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React from "react";
+import type { RootState } from "@/lib/store";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "@/lib/features/counter/counterSlice";
+
+export function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
